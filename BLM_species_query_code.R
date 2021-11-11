6b77b1fa754b0d6111b06dde832dc5af6cd3bbc6
mtarjan <max.tarjan@gmail.com>
2021-11-10 19:05:18 -0500

    updated file paths and added query for year 2 candidate species

--- BLM_species_query_code.R
+++ BLM_species_query_code.R
@@ -8,25 +8,24 @@
 library(tidyverse)
 library(readxl)
 #library(dplyr)
-blm.spp<-read_excel("BLM - Information for T & E Strategic Decision-Making - April 2021.xlsx", sheet =
+blm.spp<-read_excel("Data/BLM - Information for T & E Strategic Decision-Making - April 2021.xlsx", sheet =
                      "BLM SSS Information by State", col_names = T, skip = 1) %>% data.frame()
 blm.spp<-blm.spp[,1:11]
 
 ##read Regan's spreadsheet of Mobi models
-mobi<-read_excel("MoBI Modeling Summary by Species January 2021.xlsx", sheet =
+mobi<-read_excel("Data/MoBI Modeling Summary by Species January 2021.xlsx", sheet =
                    "MoBI_Model_Assessment", col_names = T, skip = 2) %>% data.frame()
 mobi<-mobi[,1:30]
 colnames(mobi)[1:6] <- c("Element.Global.ID", "global.id.2018", "Cutecode", "broad.group", "taxonomic.Group", "scientific.Name")
 
 ##read Year 1 BLM species list
-blm.spp.y1<-read_excel("Year1-models-delivered_20211005.xlsx", sheet =
+blm.spp.y1<-read_excel("Data/Year1-models-delivered_20211005.xlsx", sheet =
                          "Sheet1", col_names = T, skip = 0) %>% data.frame()
 
 ##add mobi model info to blm species list
 blm.mobi<-dplyr::left_join(x = blm.spp, y = mobi)
 
 ##get list of animal species for modeling
-
 animal.list <- subset(blm.mobi, 
        Taxonomic.Group != "Plant"
        #& Global.Rank..18July2020. %in% c("G1", "G2", "G3", "G4", "G5")
@@ -40,4 +39,20 @@ animal.list <- subset(blm.mobi,
        )
 dim(animal.list)
 
-write.csv(animal.list, file = "BLM_animal_models_list_8Nov2021.csv", row.names=F)
+write.csv(animal.list, file = "BLM_animal_models_list_10Nov2021.csv", row.names=F)
+
+##get a list of species to model in Year 2
+yr2.spp <- subset(blm.mobi, 
+                      Global.Rank..18July2020. %in% c("G1", "G2", "G3", "G4", "G5")
+                      & as.numeric(Occurrences.on.BLM.Lands..West....Total.Occurrences.Rangewide) >= 0.25
+                      & !(Scientific.Name %in% unique(blm.spp.y1$scientific_name))
+                      & Total.Occurrences.Rangewide > 5 ##or 10
+                      & !(ESA.Status..18Jul2020. %in% c("-", "DL"))
+                      #& Included.in.MoBI == "yes"
+                      #& Overal.All.Confidence == "Medium"
+                      #& Model.Review %in% c("High", "Medium", "Low")
+                      #& Validation.Stats %in% c("High", "Medium")
+)
+dim(yr2.spp)
+
+write.csv(yr2.spp, file = "BLM_year2_species_list_10Nov2021.csv", row.names=F)
