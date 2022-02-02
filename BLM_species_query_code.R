9370dc8e7ba57ec9e79c186353b163252055dcbb
mtarjan <max.tarjan@gmail.com>
2022-02-02 16:42:45 -0500

    updated criteria

--- BLM_species_query_code.R
+++ BLM_species_query_code.R
@@ -30,39 +30,27 @@ blm.spp.y1<-read_excel("Data/BLMSSS-Year1-models-delivered_20211005.xlsx", sheet
 ##add mobi model info to blm species list
 blm.mobi<-dplyr::left_join(x = blm.spp, y = mobi)
 
-##get list of animal species for modeling
-animal.list <- subset(blm.mobi, 
-       Taxonomic.Group != "Plant"
-       #& Global.Rank..18July2020. %in% c("G1", "G2", "G3", "G4", "G5")
-       & Global.Rank..18July2020. %in% c("G1", "G2")
-       & as.numeric(Occurrences.on.BLM.Lands..West....Total.Occurrences.Rangewide) >= 0.2
-       #& !(Scientific.Name %in% unique(blm.spp.y1$scientific_name))
-       #& Total.Occurrences.Rangewide > 5 ##or 10
-       & !(ESA.Status..18Jul2020. %in% c("-", "DL"))
-       #& Overal.All.Confidence == "Medium"
-       #& Model.Review %in% c("High", "Medium", "Low")
-       #& Validation.Stats %in% c("High")
-       )
-dim(animal.list)
-
-#write.csv(animal.list, file = "BLM_animal_models_list_10Nov2021.csv", row.names=F)
-
 ##get a list of species to model in Year 2
 yr2.spp <- subset(blm.mobi, 
                       (str_detect(Global.Rank..18July2020., "G1") | str_detect(Global.Rank..18July2020., "G2"))
                       #& Taxonomic.Group != "Plant"
-                      & (str_length(states) >= 2 | Taxonomic.Group != "Plant")
-                      & as.numeric(Prop.on.BLM.Lands.West) >= 0.2
+                      #& str_length(states) >2
+                      & (str_length(states) >2 | Taxonomic.Group != "Plant")
+                      & as.numeric(Prop.on.BLM.Lands.West) >= 0.25
                       & !(Scientific.Name %in% unique(blm.spp.y1$scientific_name))
-                      & Total.Occurrences.Rangewide >= 2 ##guessing at the number
+                      & Total.Occurrences.Rangewide >= 5 ##guessing at the number
                       & !(ESA.Status..18Jul2020. %in% c("-", "DL"))
-                      & Included.in.MoBI == "yes"
+                      #& Included.in.MoBI == "yes"
+                      #& !is.na(Included.in.MoBI)
                       #& Overal.All.Confidence == "Medium"
                       #& Model.Review %in% c("High", "Medium", "Low")
                       #& Validation.Stats %in% c("High", "Medium")
 )
 dim(yr2.spp)
 dim(subset(yr2.spp, Taxonomic.Group != "Plant"))
-subset(yr2.spp, select=c("Taxonomic.Group", "Common.Name", "Global.Rank..18July2020.", "ESA.Status..18Jul2020.", "Prop.on.BLM.Lands.West", "states"))
+subset(yr2.spp, select=c("Taxonomic.Group", "Common.Name", "Global.Rank..18July2020.", "ESA.Status..18Jul2020.", "Prop.on.BLM.Lands.West", "states"), Taxonomic.Group!="Plant") %>% arrange(Common.Name)
 
-write.csv(yr2.spp, file = "BLM_year2_species_list_20220201.csv", row.names=F)
+data.write<-subset(yr2.spp, select=c("Element.Global.ID", "Taxonomic.Group", "Scientific.Name", "Common.Name", "Global.Rank..18July2020.", "ESA.Status..18Jul2020.", "Prop.on.BLM.Lands.West", "states")) %>% arrange(Common.Name)
+names(data.write)<-c("Element.Global.ID", "Taxonomic.Group", "Scientific.Name", "Common.Name", "Global.Rank", "ESA.Status", "Prop.on.BLM.Lands.West", "States")
+
+write.csv(data.write, file = "BLM_year2_species_list_20220201.csv", row.names=F)
